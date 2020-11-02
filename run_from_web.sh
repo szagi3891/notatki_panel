403e18da310d400bed0cf7c677a3be5b7eb5ab97
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2020-11-02 17:08:37 +0100

    dist

--- run_from_web.sh
+++
@@ -1,3 +0,0 @@
-curl -L https://github.com/szagi3891/notatki_panel/blob/master/dist/server.js?raw=true | CLIENT_URL=https://github.com/szagi3891/notatki_panel/blob/master/dist/client.js?raw=true node
-
-
--- run_from_web_local.sh
+++
@@ -1,5 +0,0 @@
-./build.sh
-
-cat ./dist/server.js | CLIENT_URL=https://github.com/szagi3891/notatki_panel/blob/master/dist/client.js?raw=true node
-
-
