7a275cc6b69229cb938b3ef1e096e9cb17b8dbd7
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2021-01-31 12:15:53 +0100

    fetch

--- panel/client/src/app/state.rs
+++ panel/client/src/app/state.rs
@@ -7,9 +7,55 @@ use vertigo::{
         Computed,
         Dependencies,
         AutoMap
-    }
+    },
+    // utils::BoxRefCell,
 };
 use std::sync::Arc;
+
+#[derive(PartialEq)]
+struct NodeFetch {
+    path: Vec<String>,
+    driver: DomDriver
+}
+
+impl NodeFetch {
+    fn new(path: Vec<String>, driver: DomDriver) -> NodeFetch {
+        NodeFetch {
+            path,
+            driver
+        }
+    }
+
+    async fn get(&self) -> Result<DataNode, String> {
+        let url = format!("/fetch_node");
+        let body = ServerFetchNodePost {
+            path: self.path.clone(),
+        };
+        
+        let body_str = serde_json::to_string(&body).unwrap();
+
+        let response = self.driver.fetch(FetchMethod::GET, url, None, Some(body_str)).await;
+
+        match response {
+            Ok(response) => {
+                match serde_json::from_str::<DataNode>(response.as_str()) {
+                    Ok(data_node) => {
+                        log::info!("odpowiedź z serwera {:?}", data_node);
+                        Ok(data_node)
+                    },
+                    Err(err) => {
+                        log::error!("Error parsing response: {}", err);
+                        Err(err.to_string())
+                    }
+                }
+            },
+            Err(_) => {
+                log::error!("Error fetch");
+                Err("Error fetch".into())
+            }
+        }
+    }
+}
 
 #[derive(PartialEq)]
 pub enum Resource<T: PartialEq> {
@@ -18,12 +64,48 @@ pub enum Resource<T: PartialEq> {
     Failed(String),
 }
 
+#[derive(PartialEq)]
+struct Node {
+    data: Value<DataNode>,
+    fetch: NodeFetch,
+    // flag: BoxRefCell<bool>,
+}
+
+impl Node {
+    fn new(data: Value<DataNode>, fetch: NodeFetch) -> Node {
+        Node {
+            data,
+            fetch,
+        }
+    }
+
+    async fn refresh(&self) {
+        //TODO - tutaj trzeba zrobić odświeenie tego elementu
+        let new_data = self.fetch.get().await;
+    }
+}
+/*
+    root - 1
+
+        sub1
+            root[sub1] --- daje id kolejnego wezla
+    
+    Vec<numerki>
+
+
+    current node --> DataNodeIdType
+
+    jesli nie jest wybrana zadna sciezka, to zwracaj 1
+
+
+    przestawienie sciezki, to tak naprawde odrzucenie wszystkiego co jest za wskazanym elementem z prawej strony
+*/
 
 #[derive(PartialEq)]
 pub struct State {
     pub driver: DomDriver,
     pub current_path: Value<Vec<Arc<String>>>,
-    data: AutoMap<Vec<String>, Resource<DataNode>>,
+    data: AutoMap<Vec<String>, Resource<Node>>,
 }
 
 impl State {
@@ -32,17 +114,35 @@ impl State {
             let root = root.clone();
             let driver = driver.clone();
 
-            move |path: &Vec<String>| -> Computed<Resource<DataNode>> {
+            move |path: &Vec<String>| -> Computed<Resource<Node>> {
                 let value = root.new_value(Resource::Loading);
                 let result = value.to_computed();
     
-                fetch_path(&path, value, &driver);
+                let node_fetch = NodeFetch::new(path.clone(), driver.clone());
+
+                driver.spawn_local({
+                    let root = root.clone();
+
+                    async move {
+                        let result = node_fetch.get().await;
+
+                        match result {
+                            Ok(data_node) => {
+                                let inner_value = root.new_value(data_node);
+                                let node = Node::new(inner_value, node_fetch);
+                                value.set_value(Resource::Ready(node));
+                            },
+                            Err(err) => {
+                                value.set_value(Resource::Failed(err));
+                            }
+                        }
+                    }
+                });
     
                 result
             }
         };
 
-        // let start_path = vec!("aaa".into(), "bbb".into());
         let start_path = Vec::new();
 
         root.new_computed_from(State {
@@ -70,41 +170,6 @@ impl State {
         self.current_path.set_value(current);
     }
 }
-
-fn fetch_path(path: &Vec<String>, value: Value<Resource<DataNode>>, driver: &DomDriver) {
-
-    let url = format!("/fetch_node");
-    let body = ServerFetchNodePost {
-        path: path.clone(),
-    };
-
-    let body_str = serde_json::to_string(&body).unwrap();
-
-    let driver_fetch = driver.clone();
-    driver.spawn_local(async move {
-        let response = driver_fetch.fetch(FetchMethod::GET, url, None, Some(body_str)).await;
-
-        match response {
-            Ok(response) => {
-                match serde_json::from_str::<DataNode>(response.as_str()) {
-                    Ok(branch) => {
-                        log::info!("odpowiedź z serwera {:?}", branch);
-                        value.set_value(Resource::Ready(branch));
-                    },
-                    Err(err) => {
-                        log::error!("Error parsing response: {}", err);
-                        value.set_value(Resource::Failed(err.to_string()));
-                    }
-                }
-            },
-            Err(_) => {
-                log::error!("Error fetch");
-                value.set_value(Resource::Failed("Error fetch".into()));
-            }
-        }
-    });
-}
-
 
 /*
 dodać zakładki
