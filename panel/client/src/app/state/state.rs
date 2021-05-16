2dcb6c3acb3a982d7e80eba1db1afc40ea563bf7
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2021-05-16 09:24:22 +0200

    Klikanie we wcześniejszy element

--- panel/client/src/app/state/state.rs
+++ panel/client/src/app/state/state.rs
@@ -306,9 +306,13 @@ impl State {
     }
 
     pub fn set_path(&self, path: Vec<String>) {
+        let current_path = self.current_path.get_value();
+
+        let (new_current_path, new_current_item_value) = calculate_next_path(current_path.as_ref(), path);
+
         self.root.transaction(|| {
-            self.current_path.set_value(path);
-            self.current_item_value.set_value(None);
+            self.current_path.set_value(new_current_path);
+            self.current_item_value.set_value(new_current_item_value);
         });
     }
 
@@ -437,9 +441,91 @@ impl State {
         }
 
         //po wejściu do nowego katalogu, zaznaczać pierwszy element na liście ????
-        
 
         log::info!("klawisz ... {:?} ", code);
     }
 }
 
+
+fn calculate_next_path(prev_path: &[String], new_path: Vec<String>) -> (Vec<String>, Option<String>) {
+    if new_path.len() > prev_path.len() {
+        return (new_path, None);
+    }
+
+    if prev_path[0..new_path.len()] == new_path[0..] {
+        let last = prev_path.get(new_path.len());
+        let last = last.map(|item| item.clone());
+        return (new_path, last);
+    }
+
+    (new_path, None)
+}
+
+#[cfg(test)]
+fn create_vector<const N: usize>(list: [&str; N]) -> Vec<String> {
+    let mut out = Vec::new();
+
+    for item in list.iter() {
+        out.push(String::from(*item));
+    }
+
+    out
+}
+
+#[test]
+fn test_set_path() {
+    assert_eq!(
+        calculate_next_path(&create_vector(["cc1"]), create_vector([])),
+        (create_vector([]), Some(String::from("cc1")))
+    );
+
+    assert_eq!(
+        calculate_next_path(&create_vector(["aa1", "aa2", "aa3"]), create_vector([])),
+        (create_vector([]), Some("aa1".into()))
+    );
+
+    assert_eq!(
+        calculate_next_path(&create_vector(["aa1", "aa2", "aa3"]), create_vector(["aa1"])),
+        (create_vector(["aa1"]), Some("aa2".into()))
+    );
+
+    assert_eq!(
+        calculate_next_path(&create_vector(["aa1", "aa2", "aa3"]), create_vector(["aa1", "aa2"])),
+        (create_vector(["aa1", "aa2"]), Some("aa3".into()))
+    );
+
+    assert_eq!(
+        calculate_next_path(&create_vector(["aa1", "aa2", "aa3"]), create_vector(["aa1", "aa2", "aa3"])),
+        (create_vector(["aa1", "aa2", "aa3"]), None)
+    );
+
+    assert_eq!(
+        calculate_next_path(&create_vector(["aa1", "aa2", "aa3"]), create_vector(["aa1", "aa2", "aa3", "aa4"])),
+        (create_vector(["aa1", "aa2", "aa3", "aa4"]), None)
+    );
+
+    assert_eq!(
+        calculate_next_path(&create_vector(["aa1", "aa2", "aa3"]), create_vector([])),
+        (create_vector([]), Some("aa1".into()))
+    );
+
+    assert_eq!(
+        calculate_next_path(&create_vector(["aa1", "aa2", "aa3"]), create_vector(["bb1"])),
+        (create_vector(["bb1"]), None)
+    );
+
+    assert_eq!(
+        calculate_next_path(&create_vector(["aa1", "aa2", "aa3"]), create_vector(["bb1", "bb2"])),
+        (create_vector(["bb1", "bb2"]), None)
+    );
+
+    assert_eq!(
+        calculate_next_path(&create_vector(["aa1", "aa2", "aa3"]), create_vector(["bb1", "bb2", "bb3"])),
+        (create_vector(["bb1", "bb2", "bb3"]), None)
+    );
+
+    assert_eq!(
+        calculate_next_path(&create_vector(["aa1", "aa2", "aa3"]), create_vector(["bb1", "bb2", "bb3", "bb4"])),
+        (create_vector(["bb1", "bb2", "bb3", "bb4"]), None)
+    );
+}
