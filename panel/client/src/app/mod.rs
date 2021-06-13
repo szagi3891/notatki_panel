639542b33d914c2cd04f44eae5e2e77033e6e256
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2021-06-13 09:21:53 +0200

    Callback builder

--- panel/client/src/app/mod.rs
+++ panel/client/src/app/mod.rs
@@ -2,6 +2,7 @@ use vertigo::{
     DomDriver,
     VDomElement,
     computed::{
+        Value,
         Computed,
         Dependencies,
     },
@@ -27,7 +28,85 @@ pub enum View {
     //TODO - zmiana nazwy
 }
 
+#[derive(Clone)]
+struct CallbackBuilder {
+    root: Dependencies,
+    driver: DomDriver,
+    state_data: StateData,
+    current_view: Value<View>,
+}
 
+impl CallbackBuilder {
+    pub fn new(
+        root: &Dependencies,
+        driver: &DomDriver,
+        state_data: &StateData,
+        current_view: &Value<View>
+    ) -> CallbackBuilder {
+        CallbackBuilder {
+            root: root.clone(),
+            driver: driver.clone(),
+            state_data: state_data.clone(),
+            current_view: current_view.clone(),
+        }
+    }
+
+    pub fn redirect_to_content(&self) -> Callback<Vec<std::string::String>> {
+        let callback = self.clone();
+        
+        Callback::new(move |path: Vec<String>| {
+
+            let content = callback.state_data.get_content_from_path(&path);
+
+            match content {
+                CurrentContent::File { file_hash, content, ..} => {
+
+                    let root = callback.root.clone();
+
+                    let callback_redirect_to_index = callback.redirect_to_index();
+                    let callback_redirect_to_index_with_root_refresh = callback.redirect_to_index_with_root_refresh();
+
+                    let state = edit_content::State::new(
+                        path,
+                        file_hash,
+                        content.as_ref().clone(),
+                        &root,
+                        &callback.driver,
+                        callback_redirect_to_index,
+                        callback_redirect_to_index_with_root_refresh
+                    );
+
+                    callback.current_view.set_value(View::EditContent {
+                        state: root.new_computed_from(state)
+                    });
+                },
+                CurrentContent::Dir { .. } => {
+                    log::error!("Oczekiwano pliku, znaleziono katalog");
+                },
+                CurrentContent::None => {
+                    log::error!("Oczekiwano pliku, nic nie znaleziono");
+                }
+            }
+        })
+    }
+
+    pub fn redirect_to_index(&self) -> Callback<()> {
+        let current_view = self.current_view.clone();
+        Callback::new(move |_| {
+            current_view.set_value(View::Index);
+        })
+    }
+
+    pub fn redirect_to_index_with_root_refresh(&self) -> Callback<()> {
+        let current_view = self.current_view.clone();
+        let state_data = self.state_data.clone();
+
+        Callback::new(move |_| {
+            state_data.state_root.refresh();
+            current_view.set_value(View::Index);
+        })
+    }
+}
 
 #[derive(PartialEq)]
 pub struct State {
@@ -42,73 +121,14 @@ impl State {
         let current_view = root.new_value(View::Index);
         let current_view_computed = current_view.to_computed();
 
-        let state_view_index = {
-            let current_view = current_view.clone();
-
-            let callback_redirect_to_content = {
-                let root = root.clone();
-                let driver = driver.clone();
-                let state_data = state_data.clone();
-                
-                Callback::new(move |path: Vec<String>| {
-
-                    let content = state_data.get_content_from_path(&path);
-
-                    match content {
-                        CurrentContent::File { file_hash, content, ..} => {
-
-                            let root = root.clone();
-
-                            let callback_redirect_to_index = {
-                                let current_view = current_view.clone();
-                                Callback::new(move |_| {
-                                    current_view.set_value(View::Index);
-                                })
-                            };
-
-                            let callback_redirect_to_index_with_root_refresh = {
-                                let current_view = current_view.clone();
-                                let state_data = state_data.clone();
-
-                                Callback::new(move |_| {
-                                    state_data.state_root.refresh();
-                                    current_view.set_value(View::Index);
-                                })
-                            };
-
-                            let state = edit_content::State::new(
-                                path,
-                                file_hash,
-                                content.as_ref().clone(),
-                                &root,
-                                &driver,
-                                callback_redirect_to_index,
-                                callback_redirect_to_index_with_root_refresh
-                            );
-
-                            current_view.set_value(View::EditContent {
-                                state: root.new_computed_from(state)
-                            });
-                        },
-                        CurrentContent::Dir { .. } => {
-                            log::error!("Oczekiwano pliku, znaleziono katalog");
-                        },
-                        CurrentContent::None => {
-                            log::error!("Oczekiwano pliku, nic nie znaleziono");
-                        }
-                    }
-                })
-            };
-
-            index::State::new(
-                root,
-                state_data.clone(),
-                callback_redirect_to_content
-            )
-        };
+        let callback = CallbackBuilder::new(root, driver, &state_data, &current_view);
 
         root.new_computed_from(State {
-            state_view_index,
+            state_view_index: index::State::new(
+                root,
+                state_data.clone(),
+                callback.redirect_to_content(),
+            ),
             current_view: current_view_computed,
         })
     }
@@ -124,7 +144,7 @@ pub fn render(state: &Computed<State>) -> VDomElement {
         View::Index => {
             index::render(&state_value.state_view_index)
         },
-        View::EditContent { state }=> {
+        View::EditContent { state } => {
             edit_content::render(state)
         },
         // View::NewContent { state } => {
