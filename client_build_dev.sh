a358aefceb038a1006e4e29758ea26513d970e77
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2021-11-16 07:52:29 +0100

    cargo-make

--- client_build_dev.sh
+++
@@ -1,5 +0,0 @@
-rm -Rf ./build
-wasm-pack build panel/client --no-typescript --target web --out-dir ../../build --out-name app --dev
-
-rm ./build/.gitignore
-rm ./build/package.json
--- server_start.sh
+++
@@ -1,7 +0,0 @@
-export RUST_LOG=info
-export HTTP_HOST=0.0.0.0
-export HTTP_PORT=4000
-export GIT_REPO=/Users/grzegorzszeliga/Desktop/notatki_main/notatki
-
-cargo run --bin server
-
