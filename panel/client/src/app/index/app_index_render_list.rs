7313d26526723d6b11946c8a5cc7d76e06f72f17
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2022-04-03 21:55:48 +0200

    render_list

--- panel/client/src/app/index/app_index_render_list.rs
+++ panel/client/src/app/index/app_index_render_list.rs
@@ -1,11 +1,10 @@
 use vertigo::dev::NodeRefs;
 use vertigo::{
     Css,
-    VDomElement, Resource,
+    VDomElement,
 };
 use vertigo::{css, html};
-use crate::components::icon;
-use crate::data::{ListItem, Data, CurrentContent};
+use crate::components::list_items;
 
 use super::AppIndex;
 
@@ -13,89 +12,6 @@ fn css_wrapper() -> Css {
     css!("
         flex-grow: 1;
         overflow-y: scroll;
-    ")
-}
-
-fn css_normal(is_select: bool) -> Css {
-    let css = css!("
-        display: flex;
-        border-bottom: 1px solid #c0c0c0;
-        padding: 3px 0;
-
-        cursor: pointer;
-    ");
-
-    if is_select {
-        return css.push_str("
-            background-color: #c0c0c0;
-        ");
-    }
-
-    css
-}
-
-fn icon_arrow_wrapper() -> Css {
-    css!("
-        flex-shrink: 0;
-        width: 8px;
-        height: 16px;
-        position: relative;
-    ")
-}
-
-fn icon_wrapper_svg() -> Css {
-    css!("
-        flex-shrink: 0;
-        width: 16px;
-        height: 16px;
-        position: relative;
-        left: -4px;
-    ")
-}
-
-//https://css.gg/play-button
-
-fn icon_arrow(show: bool) -> VDomElement {
-    if show {
-        html! {
-            <div css={icon_arrow_wrapper()}>
-                <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    css={icon_wrapper_svg()}
-                >
-                    <path d="M15 12.3301L9 16.6603L9 8L15 12.3301Z" fill="currentColor" />
-                </svg>
-            </div>
-        }
-    } else {
-        html! {
-            <div css={icon_arrow_wrapper()}></div>
-        }
-    }
-}
-
-fn label_css(prirority: u8) -> Css {
-    if prirority == 2 {
-        return css!("
-            padding-left: 3px;
-            color: green;
-        ");
-    }
-
-    if prirority == 1 {
-        return css!("
-            padding-left: 3px;
-        ");
-    }
-
-    css!("
-        padding-left: 3px;
-        opacity: 0.5;
-        text-decoration: line-through;
     ")
 }
 
@@ -140,89 +56,11 @@ fn dom_apply(node_refs: &NodeRefs) {
     }
 }
 
-fn remove_first(chars: &[char]) -> &[char] {
-    if let Some((name_item, rest_path)) = chars.split_first() {
-        if *name_item == '_' {
-            return rest_path;
-        }
-    }
-
-    chars
-}
-
-fn remove_prefix(name: &String) -> String {
-    let chars = name.chars().collect::<Vec<char>>();
-
-    let chars = remove_first(&chars);
-    let chars = remove_first(chars);
-
-    let mut out: String = String::new();
-
-    for char in chars {
-        out.push(*char);
-    }
-
-    out
-}
-
-fn render_list_items(data: &Data, dir: &Vec<String>, current_item: &Option<String>) -> Vec<VDomElement> {
-    let current = data.git.dir_list(dir);
-
-    let list = match current {
-        Resource::Ready(list) => list.get_list(),
-        _ => {
-            return Vec::new();
-        }
-    };
-
-    let mut out: Vec<VDomElement> = Vec::new();
-
-    for item in (*list).iter() {
-        let on_click = {
-            let dir = dir.clone();
-            let tab = data.tab.clone();
-            let item = item.clone();
-
-            move || {
-                tab.redirect_to(dir.clone(), Some(item.name.clone()));
-            }
-        };
-
-        let is_select = {
-            if let Some(list_pointer) = current_item.as_ref() {
-                item.name == *list_pointer
-            } else {
-                false
-            }
-        };
-
-        if is_select {
-            out.push(html!{
-                <div on_click={on_click} css={css_normal(is_select)} dom_ref="active">
-                    {icon_arrow(is_select)}
-                    {icon::icon_render(item.dir)}
-                    <span css={label_css(item.prirority)}>{remove_prefix(&item.name)}</span>
-                </div>
-            });
-        } else {
-            out.push(html!{
-                <div on_click={on_click} css={css_normal(is_select)}>
-                    {icon_arrow(is_select)}
-                    {icon::icon_render(item.dir)}
-                    <span css={label_css(item.prirority)}>{remove_prefix(&item.name)}</span>
-                </div>
-            });
-        }
-    }
-
-    out
-}
-
 pub fn render_list(state: &AppIndex) -> VDomElement {    
     let dir = state.data.tab.dir.get_value();
     let current_item = state.app.data.tab.current_item.get_value();
 
-    let out = render_list_items(&state.data, dir.as_ref(), current_item.as_ref());
+    let out = list_items(&state.data, dir.as_ref(), current_item.as_ref());
 
     html! {
         <div css={css_wrapper()} dom_ref="wrapper" dom_apply={dom_apply}>
@@ -230,8 +68,6 @@ pub fn render_list(state: &AppIndex) -> VDomElement {
         </div>
     }
 }
-
-
 
 //Centrowanie na środku zawsze
 // let dom_apply = |node_refs: &NodeRefs| {
--- panel/client/src/components/mod.rs
+++ panel/client/src/components/mod.rs
@@ -2,6 +2,8 @@ mod button;
 pub mod new_name;
 mod alert_box;
 pub mod icon;
+mod list_items;
 
 pub use button::button;
 pub use alert_box::AlertBox;
+pub use list_items::list_items;
