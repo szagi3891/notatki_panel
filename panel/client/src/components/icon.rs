68c1a5caee6d57d8b686715dfe9a73fe37fd2405
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2022-06-24 21:50:45 +0200

    fix

--- panel/client/src/components/icon.rs
+++ panel/client/src/components/icon.rs
@@ -50,10 +50,10 @@ pub fn icon_file() -> DomElement {
     create_node("svg")
         .attr("viewBox", "0 0 16 16")
         .css(icon_file_css())
-        .child(create_node("path"))
+        .child(create_node("path")
             .attr("fill-rule", "evenodd")
             .attr("d", "M3.75 1.5a.25.25 0 00-.25.25v11.5c0 .138.112.25.25.25h8.5a.25.25 0 00.25-.25V6H9.75A1.75 1.75 0 018 4.25V1.5H3.75zm5.75.56v2.19c0 .138.112.25.25.25h2.19L9.5 2.06zM2 1.75C2 .784 2.784 0 3.75 0h5.086c.464 0 .909.184 1.237.513l3.414 3.414c.329.328.513.773.513 1.237v8.086A1.75 1.75 0 0112.25 15h-8.5A1.75 1.75 0 012 13.25V1.75z")
-
+        )
     // html! {
     //     <svg
     //         viewBox="0 0 16 16"
